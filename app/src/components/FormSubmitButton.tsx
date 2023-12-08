7d690621b669e96c2401b10678f111dfcabe629d
Marika <ferrari.marika@gmail.com>
2023-12-08 18:52:26 +0000

    Initial setup of a loading page after submitting the form

---
+++ app/src/components/FormSubmitButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ComponentProps } from "react";
+
+type FormSubmitButtonProps = {
+
+// More info about ComponentProps: https://www.totaltypescript.com/react-component-props-type-helper
+} & ComponentProps<"button">
+
+export default function FormSubmitButton() {
+
+}
