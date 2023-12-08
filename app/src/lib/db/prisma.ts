c9a5e38e511c4846944dca9f608fa6a5db607cb4
Marika <ferrari.marika@gmail.com>
2023-12-08 17:40:25 +0000

    Prisma Client globally available

---
+++ app/src/lib/db/prisma.ts
@@ -0,0 +1,17 @@
+// Now that the database is set up and Prisma initialized, we can utilize it globally adding the following code
+// The following code is slightly different from the original
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
