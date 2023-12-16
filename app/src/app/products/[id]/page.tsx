0cfa8923583ffabc622c848ce6fb28d599d45b84
Marika <ferrari.marika@gmail.com>
2023-12-16 16:38:02 +0000

    Product details page completed

--- app/src/app/products/[id]/page.tsx
+++ app/src/app/products/[id]/page.tsx
@@ -1,6 +1,11 @@
 // It's important to wrap the "id" folder into [] since we need to render the products by id
 
-import prisma from "@/lib/db/prisma"
+import prisma from "@/lib/db/prisma";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import PriceTag from "@/components/PriceTag";
+import { Metadata } from "next";
+import { cache } from "react";
 
 interface ProductPageProps {
     params: {
@@ -8,8 +13,50 @@ interface ProductPageProps {
     }
 }
 
+const getProduct = cache(async (id: string) => {
+    const product = await prisma.product.findUnique({where: {id}});
+
+    // notFound it's a build-in import from next/navigation that knows to reach the non-found.tsx in such cases (error handling)
+    if(!product) notFound();
+
+    return product;
+})
+
+export async function generateMetadata({params: {id}} : ProductPageProps): Promise<Metadata> {
+    const product = await getProduct(id);
+
+    return {
+        title: product.name + " - Shop-a-Hub",
+        description: product.description, 
+        openGraph: {
+            images: [
+                {url: product.imageUrl}
+            ],
+        }
+    }
+}
+
 export default async function ProductPage (
-    {params: {id}}: ProductPageProps
+    {params: {id}} : ProductPageProps
 ) {
-    const product = await prisma.product.findUnique({where: {id}})
+
+    const product = await getProduct(id);
+
+    return(
+        <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+            <Image 
+            src={product.imageUrl}
+            alt={product.name} 
+            width={500}
+            height={500}
+            className="rounded-lg"
+            priority
+            />
+            <div>
+                <h1 className="text-5xl font-bold">{product.name}</h1>
+                <PriceTag price={product.price} className="mt-4" />
+                <p className="py-6">{product.description}</p>
+            </div>
+        </div>
+    )
 }
