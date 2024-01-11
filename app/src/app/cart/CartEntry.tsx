36c78c7c4bc51db872d7b89243bc5141db2d63f7
Marika <ferrari.marika@gmail.com>
2024-01-11 11:31:27 +0000

    Display price and total price in cart page

--- app/src/app/cart/CartEntry.tsx
+++ app/src/app/cart/CartEntry.tsx
@@ -1,19 +1,28 @@
 "use client"
 
 import { CartItemWithProduct } from "@/lib/db/cart"
+import { formatPrice } from "@/lib/format"
 import Image from "next/image"
+import Link from "next/link"
 
 interface CartEntryProps {
     cartItem: CartItemWithProduct, 
 }
 
-export default function CartEntry({ cartItem : {product, quantity} }: CartEntryProps) {
+export default function CartEntry({ cartItem: { product, quantity } }: CartEntryProps) {
     return (
         <div>
             <div className="flex flex-wrap items-center gap-3">
-                <Image src={product.imageUrl} alt={product.name} width={200} height={200} className="rounded-lg"/>
+                <Image src={product.imageUrl} alt={product.name} width={200} height={200} className="rounded-lg" />
+                <div>
+                    <Link href={"/products/" + product.id} className="font-bold">{product.name}</Link>
+                    <div>Price: {formatPrice (product.price)}</div>
+                    <div className="flex items-center gap-3">
+                        Total: {formatPrice(product.price * quantity)}
+                    </div>
+                </div>
             </div>
-            <div className="divider"/>
+            <div className="divider" />
         </div>
     )
 }
