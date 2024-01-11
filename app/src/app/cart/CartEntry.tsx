e66c0c80b3e52869f93defdce0e0e737d42ed2b9
Marika <ferrari.marika@gmail.com>
2024-01-11 11:35:10 +0000

    Select quantity per item

--- app/src/app/cart/CartEntry.tsx
+++ app/src/app/cart/CartEntry.tsx
@@ -10,6 +10,16 @@ interface CartEntryProps {
 }
 
 export default function CartEntry({ cartItem: { product, quantity } }: CartEntryProps) {
+
+    const quantityOptions: JSX.Element[] = [];
+    for (let i = 1; i <= 99; i++) {
+        quantityOptions.push(
+            <option value={i} key={i}>
+                {i}
+            </option>
+        )
+    }
+
     return (
         <div>
             <div className="flex flex-wrap items-center gap-3">
@@ -17,6 +27,12 @@ export default function CartEntry({ cartItem: { product, quantity } }: CartEntry
                 <div>
                     <Link href={"/products/" + product.id} className="font-bold">{product.name}</Link>
                     <div>Price: {formatPrice (product.price)}</div>
+                    <div className="my-1 flex items-center gap-2">
+                        Quantity: 
+                        <select className="select select-bordered w-full max-w-[80px]">
+                            {quantityOptions}
+                        </select>
+                    </div>
                     <div className="flex items-center gap-3">
                         Total: {formatPrice(product.price * quantity)}
                     </div>
