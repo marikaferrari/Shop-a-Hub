ce7a6a3b8aabb623e2d682c60d803e98204f8c71
Marika <ferrari.marika@gmail.com>
2024-01-11 11:26:49 +0000

    Display product image in cart page

---
+++ app/src/app/cart/page.tsx
@@ -0,0 +1,19 @@
+import { getCart } from "@/lib/db/cart"
+import CartEntry from "./CartEntry";
+
+export const metadata = {
+    title: "Your Cart - Shop-a-Hub",
+}
+
+export default async function CartPage() {
+    const cart = await getCart();
+
+    return(
+        <div>
+            <h1 className="mb-6 text-3xl font-bold">Shopping Cart</h1>
+            {cart?.items.map(cartItem => (
+                <CartEntry cartItem={cartItem} key={cartItem.id} />
+            ))}
+        </div>
+    )
+}
--- app/src/app/products/[id]/actions.ts
+++ app/src/app/products/[id]/actions.ts
@@ -1,7 +1,7 @@
 // Server actions for 'add to cart' button
 'use server'
 
-import { createCart, getCart } from "@/lib/db/card"
+import { createCart, getCart } from "@/lib/db/cart"
 import prisma from "@/lib/db/prisma";
 import { revalidatePath } from "next/cache";
 
