8ddd3771118e61dae4444a9e1169fdd3d64963ff
Marika <ferrari.marika@gmail.com>
2024-01-11 15:47:39 +0000

    Pagination bar completed + number of items diplayed per page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,18 +2,38 @@ import Image from 'next/image';
 import prisma from '@/lib/db/prisma';
 import ProductCard from '@/components/ProductCard';
 import Link from 'next/link';
+import PaginationBar from '@/components/PaginationBar';
+
+interface HomeProps {
+  searchParams: { page: string }
+}
 
 // Define Home component as async function
   // Highlight newest product at the top
   // Use ProductCard component to render remaining products
-export default async function Home() {
+export default async function Home({searchParams : { page = "1" } }: HomeProps) {
+
+  const currentPage = parseInt(page);
+
+  const pageSize = 6;
+
+  // Big picture at the top
+  const heroItemCount = 1;
+
+  // Count the number of total pages
+  const totalItemCount = await prisma.product.count();
+  const totalPages = Math.ceil((totalItemCount - heroItemCount) / pageSize)
+
   // Fetch all the products from database
   const products = await prisma.product.findMany({
     // Products order by id in descending order
-    orderBy: {id: "desc"}
+    orderBy: {id: "desc"},
+    skip: (currentPage - 1) * pageSize + (currentPage === 1 ? 0 : heroItemCount),
+    take: pageSize + (currentPage === 1 ? heroItemCount : 0),
   })
   return (
-   <div>
+   <div className='flex flex-col items-center'>
+    {currentPage === 1 && (
     <div className='hero rounded-xl bg-base-200'>
       <div className='hero-content flex-col lg:flex-row'>
       <Image 
@@ -32,14 +52,18 @@ export default async function Home() {
         </div>
       </div>
     </div>
+    )}
   {/* Display a grid of product cards for the remaining products */}
     <div className='my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
-      {products.slice(1).map(product => (
+      {(currentPage === 1 ? products.slice(1) : products).map(product => (
         // ProductCard component
           // 'Key' attribute is a required prop when rendering lists of elements
         <ProductCard product={product} key={product.id} />
       ))}
     </div>
+        {totalPages > 1 && 
+    <PaginationBar currentPage={currentPage} totalPages={totalPages} />
+        }
    </div>
   )
 }
