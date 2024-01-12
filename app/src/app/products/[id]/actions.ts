c407c9ca62b63741bda1c881eded685aed124609
Marika <ferrari.marika@gmail.com>
2024-01-12 09:50:28 +0000

    Fixed anynoumous cart potential bug

--- app/src/app/products/[id]/actions.ts
+++ app/src/app/products/[id]/actions.ts
@@ -1,30 +1,40 @@
 // Server actions for 'add to cart' button
-'use server'
+"use server";
 
-import { createCart, getCart } from "@/lib/db/cart"
+import { createCart, getCart } from "@/lib/db/cart";
 import prisma from "@/lib/db/prisma";
 import { revalidatePath } from "next/cache";
 
 export async function incrementProductQuantity(productId: string) {
-    const cart = (await getCart()) ?? (await createCart());
+  const cart = (await getCart()) ?? (await createCart());
 
-    const articleInCart = cart.items.find(item => item.productId === productId);
+  const articleInCart = cart.items.find((item) => item.productId === productId);
 
-    if (articleInCart) {
-        await prisma.cartItem.update({
+  if (articleInCart) {
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: {
+          update: {
             where: { id: articleInCart.id },
-            data: { quantity: { increment: 1 }}
-        })
-    }
-    else {
-        await prisma.cartItem.create({
-            data: {
-                cartId: cart.id,
-                productId,
-                quantity: 1
-            }
-        })
-    }
+            data: { quantity: { increment: 1 } },
+          },
+        },
+      },
+    });
+  } else {
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: {
+          create: {
+            productId,
+            quantity: 1,
+          },
+        },
+      },
+    });
+  }
 
-    revalidatePath("/products/[id]");
+  revalidatePath("/products/[id]");
 }
--- app/src/lib/db/prisma.ts
+++ app/src/lib/db/prisma.ts
@@ -1,17 +1,22 @@
 // Now that the database is set up and Prisma initialized, we can utilize it globally adding the following code
 // The following code is slightly different from the original
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
+const prismaBase = globalForPrisma.prisma ?? new PrismaClient();
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const prisma = prismaBase.$extends({
+  query: {
+    cart: {
+      async update({ args, query }) {
+        args.data = { ...args.data, updatedAt: new Date() };
+        return query(args);
+      },
+    },
+  },
+});
 
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismaBase;
